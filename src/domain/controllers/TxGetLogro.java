76cd96edf441388797be7268b39e9d62baa38468
pol.bornas <pol.bornas@estudiantat.upc.edu>
2022-04-28 23:08:23 +0200

    cambios logros

---
+++ src/domain/controllers/TxGetLogro.java
@@ -0,0 +1,28 @@
+package domain.controllers;
+
+import java.util.ArrayList;
+
+import domain.Logro;
+import domain.dataCtrl.DataCtrl;
+import domain.dataCtrl.LogroDataCtrl;
+
+public class TxGetLogro {
+    private String name;
+    private Tier tier;
+    private Logro result;
+
+    public TxGetLogro(String name, Tier tier){
+        this.name = name;
+        this.tier = tier;
+    }
+
+    public void execute(){
+        DataCtrl dataCtrl = DataCtrl.getInstance();
+        LogroDataCtrl sdc = dataCtrl.getLogroDataCtrl();
+        result = sdc.select(name, tier);
+    }
+
+    public Logro getResult(){
+        return result;
+    }
+}
