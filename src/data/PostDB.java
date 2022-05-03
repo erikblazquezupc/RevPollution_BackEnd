137d58127ff9077b531695e074a9fbf7f094762e
DavidFalces <71983111+DavidFalces@users.noreply.github.com>
2022-05-03 12:55:04 +0200

    Complete PostDB. Files now ready for testing!

--- src/data/PostDB.java
+++ src/data/PostDB.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import domain.Post;
+import domain.User;
 import domain.dataCtrl.PostDataCtrl;
 
 public class PostDB implements PostDataCtrl{
@@ -24,11 +25,11 @@ public class PostDB implements PostDataCtrl{
         try {
             Class.forName("com.mysql.jdbc.Driver");
             conn = DriverManager.getConnection("jdbc:mysql://10.4.41.56:3306/RevPollution_Dev?allowPublicKeyRetrieval=true&useSSL=false", "dev", "aRqffCdBd9t!");
-            insert = conn.prepareStatement("");
-            selectAll = conn.prepareStatement("");
-            selectByDateBigger = conn.prepareStatement("");
-            selectByDateSmaller = conn.prepareStatement("");
-            selectByDateBoth = conn.prepareStatement("");
+            insert = conn.prepareStatement("INSERT INTO Posts(text, idCreator, postedOn) VALUES (?, ?, ?)");
+            selectAll = conn.prepareStatement("SELECT * FROM Posts");
+            selectByDateBigger = conn.prepareStatement("SELECT * FROM Posts WHERE timestamp > ?");
+            selectByDateSmaller = conn.prepareStatement("SELECT * FROM Posts WHERE timestamp < ?");
+            selectByDateBoth = conn.prepareStatement("SELECT * FROM Posts WHERE timestamp > ? AND timestamp < ?");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
@@ -41,10 +42,13 @@ public class PostDB implements PostDataCtrl{
         return instance;
     }
 
-    public boolean insert(Post u){
+    public boolean insert(Post p){
         try {
-            // Logic and return true
-            return true;
+            insert.setString(1, p.getText());
+            insert.setInt(2, p.getCreator().getId());
+            insert.setLong(3, p.getPostedOn());
+            int n = insert.executeUpdate();
+            if (n >= 1) return true;
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -52,9 +56,20 @@ public class PostDB implements PostDataCtrl{
     }
 
     public List<Post> selectAll(){
+        ArrayList<Post> ret = new ArrayList<Post> ();
         try {
-            // Logic and return posts
-            return new ArrayList<Post>();
+            ResultSet r = selectAll.executeQuery();
+            while(r.next()) {
+                int idCreator = r.getInt("idCreator");
+                String text = r.getString("text");
+                long postedOn = r.getLong("postedOn");
+                
+                User user = UserDB.getInstance().select(idCreator);
+
+                Post s = new Post(user, text, postedOn);
+                ret.add(s);
+            }
+            return ret;
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -62,19 +77,22 @@ public class PostDB implements PostDataCtrl{
     }
 
     public List<Post> selectByDateBigger(Integer firstDate) {
+        ArrayList<Post> ret = new ArrayList<Post> ();
         try {
-            // Logic and return posts
-            return new ArrayList<Post>();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+            ResultSet r = selectByDateBigger.executeQuery();
+            while(r.next()) {
+                selectByDateBigger.setInt(1, firstDate);
 
-    public List<Post> selectByDateBoth(Integer firstDate, Integer lastDate) {
-        try {
-            // Logic and return posts
-            return new ArrayList<Post>();
+                int idCreator = r.getInt("idCreator");
+                String text = r.getString("text");
+                long postedOn = r.getLong("postedOn");
+                
+                User user = UserDB.getInstance().select(idCreator);
+
+                Post s = new Post(user, text, postedOn);
+                ret.add(s);
+            }
+            return ret;
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -82,14 +100,49 @@ public class PostDB implements PostDataCtrl{
     }
 
     public List<Post> selectByDateSmaller(Integer lastDate) {
+        ArrayList<Post> ret = new ArrayList<Post> ();
         try {
-            // Logic and return posts
-            return new ArrayList<Post>();
+            ResultSet r = selectByDateSmaller.executeQuery();
+            while(r.next()) {
+                selectByDateSmaller.setInt(1, lastDate);
+
+                int idCreator = r.getInt("idCreator");
+                String text = r.getString("text");
+                long postedOn = r.getLong("postedOn");
+                
+                User user = UserDB.getInstance().select(idCreator);
+
+                Post s = new Post(user, text, postedOn);
+                ret.add(s);
+            }
+            return ret;
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    
+    public List<Post> selectByDateBoth(Integer firstDate, Integer lastDate) {
+        ArrayList<Post> ret = new ArrayList<Post> ();
+        try {
+            ResultSet r = selectByDateBoth.executeQuery();
+            while(r.next()) {
+                selectByDateBoth.setInt(1, firstDate);
+                selectByDateBoth.setInt(2, lastDate);
+
+                int idCreator = r.getInt("idCreator");
+                String text = r.getString("text");
+                long postedOn = r.getLong("postedOn");
+                
+                User user = UserDB.getInstance().select(idCreator);
+
+                Post s = new Post(user, text, postedOn);
+                ret.add(s);
+            }
+            return ret;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
